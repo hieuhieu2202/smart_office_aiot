6fdcdbd626c89e3bc0352a08d022c9c900d9c02e
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-09-15 23:17:25 -0700

    Improve Android build configuration compatibility

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,17 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://storage.googleapis.com/download.flutter.io")
+    }
 }
 
 include(":app")
