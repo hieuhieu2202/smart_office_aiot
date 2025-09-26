425e5b355aafca6612c64f878d093e6a56bed9d8
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-09-26 01:02:12 -0700

    Adjust EVA loader scaling

--- lib/widget/animation/loading/eva_loading_view.dart
+++ lib/widget/animation/loading/eva_loading_view.dart
@@ -5,8 +5,15 @@ import 'package:flutter/material.dart';
 import 'eva_scanner.dart';
 
 class EvaLoadingView extends StatelessWidget {
-  const EvaLoadingView({super.key, this.size = 220, this.alignment = Alignment.center, this.padding});
+  const EvaLoadingView({
+    super.key,
+    this.size = 220,
+    this.alignment = Alignment.center,
+    this.padding,
+  });
 
+  /// The natural size of the EVA robot animation. The widget will try to honor
+  /// this dimension and will only scale down if its parent provides less space.
   final double size;
   final AlignmentGeometry alignment;
   final EdgeInsetsGeometry? padding;
@@ -15,17 +22,25 @@ class EvaLoadingView extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = LayoutBuilder(
       builder: (context, constraints) {
-        final maxWidth = constraints.hasBoundedWidth ? constraints.maxWidth : size;
-        final maxHeight = constraints.hasBoundedHeight ? constraints.maxHeight : size;
+        final maxWidth = constraints.hasBoundedWidth ? constraints.maxWidth : double.infinity;
+        final maxHeight = constraints.hasBoundedHeight ? constraints.maxHeight : double.infinity;
 
-        double dimension = math.min(size, math.min(maxWidth, maxHeight));
-        if (dimension.isInfinite || dimension <= 0) {
-          dimension = size;
+        double dimension = size;
+        final available = math.min(maxWidth, maxHeight);
+        if (available.isFinite && available > 0) {
+          dimension = math.min(size, available);
         }
 
-        return SizedBox.square(
-          dimension: dimension,
-          child: EvaScanner(size: dimension),
+        return SizedBox(
+          width: dimension,
+          height: dimension,
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: SizedBox.square(
+              dimension: size,
+              child: EvaScanner(size: size),
+            ),
+          ),
         );
       },
     );
