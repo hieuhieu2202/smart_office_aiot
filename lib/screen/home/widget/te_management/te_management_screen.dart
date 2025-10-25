156e03c8bfd30c1ba62a0d49a14f576f9bcbc336
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-10-25 10:56:22 +0700

    Import HapticFeedback helpers

--- lib/screen/home/widget/te_management/te_management_screen.dart
+++ lib/screen/home/widget/te_management/te_management_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math' as math;
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
