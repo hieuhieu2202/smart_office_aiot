d34b091a738090579146f91b20460b0019ecb2a9
MinhTran2303 <tranhoangminhyg@gmail.com>
2025-10-29 14:25:01 +0700

    Use legacy tooltip properties for fl_chart

--- lib/features/lcr_machine/presentation/widgets/lcr_chart_card.dart
+++ lib/features/lcr_machine/presentation/widgets/lcr_chart_card.dart
@@ -188,8 +188,8 @@ class EmployeeStatisticsChart extends StatelessWidget {
           barTouchData: BarTouchData(
             enabled: true,
             touchTooltipData: BarTouchTooltipData(
-              tooltipBackgroundColor: Colors.black.withOpacity(0.8),
-              tooltipBorderRadius: BorderRadius.circular(8),
+              tooltipBgColor: Colors.black.withOpacity(0.8),
+              tooltipRoundedRadius: 8,
               tooltipPadding: const EdgeInsets.symmetric(
                 horizontal: 14,
                 vertical: 10,
