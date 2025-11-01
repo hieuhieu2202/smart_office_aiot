ba555bffa91b941a3cc2da0a73af461b4e39ead1
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-11-01 16:42:52 +0700

    Reduce TE top error header font size

--- lib/features/te_management/presentation/views/te_top10_error_code_screen.dart
+++ lib/features/te_management/presentation/views/te_top10_error_code_screen.dart
@@ -38,7 +38,7 @@ const Color _kTrendRepairColor = Color(0xFF00FF9C);
 
 const TextStyle _kTableHeaderStyle = TextStyle(
   color: _kTextPrimary,
-  fontSize: 12,
+  fontSize: 11,
   fontWeight: FontWeight.w700,
   letterSpacing: 0.2,
 );
