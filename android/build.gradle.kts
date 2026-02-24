1b4a3709706671fc6fd461d83a45e292c2f1f317
hieuhieu2202 <hieuhieu22022001@gmail.com>
2026-02-24 10:02:51 +0700

    update cameera

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,13 +6,11 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+    project.layout.buildDirectory.value(newBuildDir.dir(project.name))
     project.evaluationDependsOn(":app")
 }
 
